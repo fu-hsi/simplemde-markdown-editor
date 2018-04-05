908d90086f9ca1b623c343da95ae4d420ccf1430
Gautier Michelin <gm@ideesculture.com>
2018-04-05 21:28:48 +0200

    best to use in english

--- dist/upload/index.php
+++ dist/upload/index.php
@@ -1,38 +1,37 @@
 <?php
-$dossier     = 'files/';
-$fichier     = basename($_FILES['file']['name']);
-$taille_maxi = 1024 * 1024 * 20;
-$taille      = filesize($_FILES['file']['tmp_name']);
-$extensions  = array('.png', '.gif', '.jpg', '.jpeg');
-$extension   = strrchr($_FILES['file']['name'], '.');
+$folder       = 'files/';
+$file         = basename($_FILES['file']['name']);
+$max_filesize = 1024 * 1024 * 20;
+$size         = filesize($_FILES['file']['tmp_name']);
+$extensions   = array('.png', '.gif', '.jpg', '.jpeg');
+$extension    = strrchr($_FILES['file']['name'], '.');
 
-$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . "/dist/upload/files";
+$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . "/upload/files";
 
-//Début des vérifications de sécurité...
-if (!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
-{
-    $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+//Starting security checks
+if (!in_array($extension, $extensions)) {
+    //If the extension is not in the array
+    $error = 'Upload is restricted to png, gif, jpg, jpeg, txt or doc...';
 }
-if ($taille > $taille_maxi) {
-    $erreur = 'Le fichier est trop gros...';
+if ($size > $max_filesize) {
+    $error = 'Filesize is over the max filesize defined...';
 }
-if (!isset($erreur)) //S'il n'y a pas d'erreur, on upload
-{
-    //On formate le nom du fichier ici...
-    $fichier = strtr($fichier,
+
+if (!isset($error)) {
+    //No error, uploading
+    //Name reformating
+    $file = strtr($file,
         'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
         'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-    $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-    if (move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-    {
-        //echo 'Upload effectué avec succès !';
-        print json_encode(["filename" => $actual_link . "/" . $fichier]);
-    } else //Sinon (la fonction renvoie FALSE).
-    {
-        print json_encode('Echec de l\'upload !');
+    $file = preg_replace('/([^.a-z0-9]+)/i', '-', $file);
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $folder . $file)) {
+        //if true, upload has started
+        print json_encode(["filename" => $actual_link . "/" . $file]);
+    } else {
+        print json_encode('Upload has failed.');
     }
 } else {
-    print json_encode($erreur);
+    print json_encode($error);
 }
 ?>
 
